ec353d8a453cba3241a032cbcf20360a9b55d3ce
Nam Nguyen <nlnam@fossil.com>
2018-03-15 09:59:10 +0700

    Merge 2 sorted arrays into one

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-const response = require('./respon.json');
+const firstArr = [1, 3, 6];
+const secondArr = [2, 5, 6, 7];
 
-console.log(response.fName);
+/**
+ *
+ *
+ * @param {Array<Number>} first
+ * @param {Array<Number>} second
+ */
+const merge = (first, second) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < first.length && j < second.length) {
+    if (first[i] < second[j]) {
+      result.push(first[i]);
+      i += 1;
+    } else {
+      result.push(second[j]);
+      j += 1;
+    }
+  }
+
+  if (i < first.length) {
+    result.push(...first.slice(i));
+  } else {
+    result.push(...second.slice(j));
+  }
+
+  return result;
+};
+
+console.log(merge(firstArr, secondArr));
