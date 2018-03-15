f98ff549aabd0ba92217d02d7e7042c40f8d27ee
Nam Nguyen <nlnam@fossil.com>
2018-03-15 12:02:20 +0700

    File upload

--- main.js
+++ main.js
@@ -1,68 +1,41 @@
-// eslint-disable-next-line
-let dragSrcEl = null
+window.URL = window.URL || window.webkitURL
 
-function handleDragStart(e) {
-  // Target (this) element is the source node.
-  this.style.opacity = '0.4'
+const fileSelect = document.getElementById('fileSelect')
+const fileElem = document.getElementById('fileElem')
+const fileList = document.getElementById('fileList')
 
-  dragSrcEl = this
+fileSelect.addEventListener(
+  'click',
+  (e) => {
+    if (fileElem) {
+      fileElem.click()
+    }
+    e.preventDefault() // prevent navigation to "#"
+  },
+  false,
+)
 
-  e.dataTransfer.effectAllowed = 'move'
-  e.dataTransfer.setData('text/html', this.innerHTML)
-}
+function handleFiles(files) {
+  if (!files.length) {
+    fileList.innerHTML = '<p>No files selected!</p>'
+  } else {
+    fileList.innerHTML = ''
+    const list = document.createElement('ul')
+    fileList.appendChild(list)
+    for (let i = 0; i < files.length; i++) {
+      const li = document.createElement('li')
+      list.appendChild(li)
 
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault() // Necessary. Allows us to drop.
+      const img = document.createElement('img')
+      img.src = window.URL.createObjectURL(files[i])
+      img.height = 60
+      img.onload = function () {
+        window.URL.revokeObjectURL(this.src)
+      }
+      li.appendChild(img)
+      const info = document.createElement('span')
+      info.innerHTML = `${files[i].name}: ${files[i].size} bytes`
+      li.appendChild(info)
+    }
   }
-
-  e.dataTransfer.dropEffect = 'move' // See the section on the DataTransfer object.
-
-  return false
 }
-
-function handleDragEnter() {
-  // this / e.target is the current hover target.
-  this.classList.add('over')
-}
-
-function handleDragLeave() {
-  this.classList.remove('over') // this / e.target is previous target element.
-}
-
-function handleDrop(e) {
-  // this/e.target is current target element.
-
-  if (e.stopPropagation) {
-    e.stopPropagation() // Stops some browsers from redirecting.
-  }
-
-  // Don't do anything if dropping the same column we're dragging.
-  if (dragSrcEl !== this) {
-    // Set the source column's HTML to the HTML of the column we dropped on.
-    dragSrcEl.innerHTML = this.innerHTML
-    this.innerHTML = e.dataTransfer.getData('text/html')
-  }
-
-  dragSrcEl.style.opacity = 1
-
-  return false
-}
-
-function handleDragEnd() {
-  // this/e.target is the source node.
-  // eslint-disable-next-line
-  cols.forEach(col => {
-    col.classList.remove('over')
-  })
-}
-
-const cols = document.querySelectorAll('#columns .column');
-[].forEach.call(cols, (col) => {
-  col.addEventListener('dragstart', handleDragStart, false)
-  col.addEventListener('dragenter', handleDragEnter, false)
-  col.addEventListener('dragover', handleDragOver, false)
-  col.addEventListener('dragleave', handleDragLeave, false)
-  col.addEventListener('drop', handleDrop, false)
-  col.addEventListener('dragend', handleDragEnd, false)
-})
