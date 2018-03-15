a9f9f7caaa961c57547e1bbe189f59611ef8ce00
Nam Nguyen <nlnam@fossil.com>
2018-03-15 09:59:58 +0700

    Add eslint rule for semi and console

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const firstArr = [1, 3, 6];
-const secondArr = [2, 5, 6, 7];
+const firstArr = [1, 3, 6]
+const secondArr = [2, 5, 6, 7]
 
 /**
  *
@@ -8,27 +8,27 @@ const secondArr = [2, 5, 6, 7];
  * @param {Array<Number>} second
  */
 const merge = (first, second) => {
-  const result = [];
-  let i = 0;
-  let j = 0;
+  const result = []
+  let i = 0
+  let j = 0
 
   while (i < first.length && j < second.length) {
     if (first[i] < second[j]) {
-      result.push(first[i]);
-      i += 1;
+      result.push(first[i])
+      i += 1
     } else {
-      result.push(second[j]);
-      j += 1;
+      result.push(second[j])
+      j += 1
     }
   }
 
   if (i < first.length) {
-    result.push(...first.slice(i));
+    result.push(...first.slice(i))
   } else {
-    result.push(...second.slice(j));
+    result.push(...second.slice(j))
   }
 
-  return result;
-};
+  return result
+}
 
-console.log(merge(firstArr, secondArr));
+console.log(merge(firstArr, secondArr))
